13045a8a24716edcb7d9f5f1dcd559e749eb931a
Sheen Capadngan <sheensantoscapadngan@gmail.com>
2024-05-03 23:32:17 +0800

    feature: initial secret loader version

---
+++ index.js
@@ -0,0 +1,54 @@
+import core from "@actions/core";
+import { UALogin, getRawSecrets } from "./infisical.js";
+import fs from "fs/promises";
+
+try {
+  const UAClientId = core.getInput("client-id");
+  const UAClientSecret = core.getInput("client-secret");
+  const domain = core.getInput("domain");
+  const envSlug = core.getInput("env-slug");
+  const projectSlug = core.getInput("project-slug");
+  const secretPath = core.getInput("secret-path");
+  const exportType = core.getInput("export-type");
+  const fileOutputPath = core.getInput("file-output-path");
+
+  // get infisical token using UA credentials
+  const infisicalToken = await UALogin({
+    clientId: UAClientId,
+    clientSecret: UAClientSecret,
+  });
+
+  // get secrets from Infisical using input params
+  const keyValueSecrets = await getRawSecrets({
+    domain,
+    envSlug,
+    infisicalToken,
+    projectSlug,
+    secretPath,
+  });
+
+  // export fetched secrets
+  if (exportType === "env") {
+    // Write the secrets to action ENV
+    keyValueSecrets.entries(([key, value]) => {
+      core.setSecret(value);
+      core.exportVariable(key, value);
+    });
+    core.info("Injected secrets as environment variables");
+  } else if (exportType === "file") {
+    // Write the secrets to a .env file at the specified path
+    const envContent = Object.keys(keyValueSecrets)
+      .map((key) => `${key}=${obj[key]}`)
+      .join("\n");
+
+    try {
+      await fs.writeFile(fileOutputPath, envContent);
+    } catch (err) {
+      core.error(`Error writing .env file: ${err}`);
+      throw err;
+    }
+    core.info(`.env file was saved successfully to ${fileOutputPath}`);
+  }
+} catch (error) {
+  core.setFailed(error.message);
+}
---
+++ infisical.js
@@ -0,0 +1,73 @@
+import axios from "axios";
+import querystring from "querystring";
+
+export const UALogin = async ({ clientId, clientSecret, domain }) => {
+  const loginData = querystring.stringify({
+    clientId,
+    clientSecret,
+  });
+
+  try {
+    const response = await axios({
+      method: "post",
+      url: `${domain}/api/v1/auth/universal-auth/login`,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: loginData,
+    });
+    return response.data.accessToken;
+  } catch (err) {
+    console.error("Error:", error.message);
+    throw err;
+  }
+};
+
+export const getRawSecrets = async ({
+  domain,
+  envSlug,
+  infisicalToken,
+  projectSlug,
+  secretPath,
+}) => {
+  try {
+    const response = await axios({
+      method: "get",
+      url: `${domain}/api/v3/secrets/raw`,
+      headers: {
+        Authorization: `Bearer ${infisicalToken}`,
+      },
+      params: {
+        secretPath,
+        environment: envSlug,
+        include_imports: true,
+        recursive: true,
+        workspaceSlug: projectSlug,
+      },
+    });
+
+    const keyValueSecrets = response.data.secrets.reduce(
+      (accumulator, secret) => {
+        accumulator[secret.secretKey] = secret.secretValue;
+        return accumulator;
+      },
+      {}
+    );
+
+    // process imported secrets
+    const imports = response.data.imports;
+    for (let x = imports.length - 1; x >= 0; x--) {
+      const importedSecrets = imports[x].secrets;
+      importedSecrets.forEach((secret) => {
+        if (keyValueSecrets[secret.secretKey] === undefined) {
+          keyValueSecrets[secret.secretKey] = secret.secretValue;
+        }
+      });
+    }
+
+    return keyValueSecrets;
+  } catch (err) {
+    console.error("Error:", error.message);
+    throw err;
+  }
+};
