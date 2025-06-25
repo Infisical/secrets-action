2a5063bf78e7bd2621569068b56bbdd9ca1020dd
carlosmonastyrski <charly.monastyrski@gmail.com>
2025-06-25 15:54:24 -0300

    Change enum to const

--- constants.js
+++ constants.js
@@ -1,8 +1,8 @@
 export const AWS_TOKEN_METADATA_URI = "http://169.254.169.254/latest/api/token";
 export const AWS_IDENTITY_DOCUMENT_URI = "http://169.254.169.254/latest/dynamic/instance-identity/document";
 
-export enum AuthMethod {
-    Universal = "universal",
-    Oidc = "oidc",
-    AwsIam = "aws-iam"
+export const AuthMethod = {
+    Universal: "universal",
+    Oidc: "oidc",
+    AwsIam: "aws-iam"
 }
