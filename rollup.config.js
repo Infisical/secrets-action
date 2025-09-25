632d026dbd840c4e9045e5cd77a9599ab1f12526
Daniel Hougaard <daniel@infisical.com>
2025-09-26 01:41:08 +0400

    convert to typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,28 @@
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import json from "@rollup/plugin-json";
+import typescript from "@rollup/plugin-typescript";
 
 /** @type {import('rollup').RollupOptions} */
 const config = {
-  input: "src/index.js",
+  input: "src/index.ts",
   output: {
-    esModule: true,
+    esModule: false,
     inlineDynamicImports: true,
-    file: "dist/index.js",
-    format: "es",
+    file: "dist/index.cjs",
+    format: "cjs",
     sourcemap: true,
+    banner: "#!/usr/bin/env node",
   },
-  plugins: [commonjs(), nodeResolve({ preferBuiltins: true }), json()],
+  plugins: [
+    typescript({
+      declaration: false,
+      tsconfig: "./tsconfig.json"
+    }),
+    commonjs(),
+    nodeResolve({ preferBuiltins: true }),
+    json()
+  ],
 }
 
 export default config;
---
+++ src/index.ts
@@ -0,0 +1,133 @@
+import core from "@actions/core";
+import { UALogin, getRawSecrets, oidcLogin, awsIamLogin, createAxiosInstance } from "./infisical";
+import fs from "fs/promises";
+import { AuthMethod } from "./constants";
+
+function parseHeadersInput(inputKey: string) {
+	const rawHeadersString = core.getInput(inputKey) || "";
+
+	const headerStrings = rawHeadersString
+		.split("\n")
+		.map(line => line.trim())
+		.filter(line => line !== "");
+
+	const parsedHeaderStrings = headerStrings.reduce(
+		(obj, line) => {
+			const seperator = line.indexOf(":");
+			const key = line.substring(0, seperator).trim().toLowerCase();
+			const value = line.substring(seperator + 1).trim();
+			if (obj[key]) {
+				obj[key] = [obj[key], value].join(", ");
+			} else {
+				obj[key] = value;
+			}
+			return obj;
+		},
+		{} as Record<string, string>
+	);
+
+	return parsedHeaderStrings;
+}
+
+const main = async () => {
+	try {
+		const method = core.getInput("method");
+		const UAClientId = core.getInput("client-id");
+		const UAClientSecret = core.getInput("client-secret");
+		const identityId = core.getInput("identity-id");
+		const oidcAudience = core.getInput("oidc-audience");
+		const domain = core.getInput("domain");
+		const envSlug = core.getInput("env-slug");
+		const projectSlug = core.getInput("project-slug");
+		const secretPath = core.getInput("secret-path");
+		const exportType = core.getInput("export-type");
+		const fileOutputPath = core.getInput("file-output-path");
+		const shouldIncludeImports = core.getBooleanInput("include-imports");
+		const shouldRecurse = core.getBooleanInput("recursive");
+		const extraHeaders = parseHeadersInput("extra-headers");
+
+		// get infisical token using credentials
+		let infisicalToken;
+
+		const axiosInstance = createAxiosInstance(domain, extraHeaders);
+
+		switch (method) {
+			case AuthMethod.Universal: {
+				if (!(UAClientId && UAClientSecret)) {
+					throw new Error("Missing universal auth credentials");
+				}
+				infisicalToken = await UALogin({
+					axiosInstance,
+					clientId: UAClientId,
+					clientSecret: UAClientSecret
+				});
+				break;
+			}
+			case AuthMethod.Oidc: {
+				if (!identityId) {
+					throw new Error("Missing identity ID for OIDC auth");
+				}
+				infisicalToken = await oidcLogin({
+					axiosInstance,
+					identityId,
+					oidcAudience
+				});
+				break;
+			}
+			case AuthMethod.AwsIam: {
+				if (!identityId) {
+					throw new Error("Missing identity ID for AWS IAM auth");
+				}
+				infisicalToken = await awsIamLogin({
+					axiosInstance,
+					identityId
+				});
+				break;
+			}
+			default:
+				throw new Error(`Invalid authentication method: ${method}`);
+		}
+
+		// get secrets from Infisical using input params
+		const keyValueSecrets = await getRawSecrets({
+			axiosInstance,
+			envSlug,
+			infisicalToken,
+			projectSlug,
+			secretPath,
+			shouldIncludeImports,
+			shouldRecurse
+		});
+
+		core.debug(`Exporting the following envs", ${JSON.stringify(Object.keys(keyValueSecrets))}`);
+
+		// export fetched secrets
+		if (exportType === "env") {
+			// Write the secrets to action ENV
+			Object.entries(keyValueSecrets).forEach(([key, value]) => {
+				core.setSecret(value);
+				core.exportVariable(key, value);
+			});
+			core.info("Injected secrets as environment variables");
+		} else if (exportType === "file") {
+			// Write the secrets to a file at the specified path
+			const fileContent = Object.keys(keyValueSecrets)
+				.map(key => `${key}='${keyValueSecrets[key]}'`)
+				.join("\n");
+
+			try {
+				const filePath = `${process.env.GITHUB_WORKSPACE}${fileOutputPath}`;
+				core.info(`Exporting secrets to ${filePath}`);
+				await fs.writeFile(filePath, fileContent);
+			} catch (err) {
+				core.error(`Error writing file: ${(err as Error)?.message}`);
+				throw err;
+			}
+			core.info("Successfully exported secrets to file");
+		}
+	} catch (err) {
+		core.setFailed((err as Error)?.message);
+	}
+};
+
+main();
